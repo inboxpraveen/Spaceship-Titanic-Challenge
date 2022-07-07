97db2d0af3b03edd9c5905335d064c7f31383bbb
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2022-07-07 23:54:44 +0530

    updated download model function

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from utils import *
+import kaggle
 
 if not os.path.exists(DATA_PATH):
     os.makedir(DATA_PATH)
@@ -9,7 +10,13 @@ if not os.path.exists(MODEL_PATH):
 if not os.path.exists(TEMP_PATH):
     os.makedir(TEMP_PATH)
 
-def download_data(dataset_name :str = "") -> bool:
-    ...
 
+def download_data(dataset_name :str = "") -> bool:
+    
+    kaggle.api.authenticate()
+    kaggle.api.dataset_download_files(dataset_name, path=DATA_PATH, unzip=True)
+
+
+if __name__ == "__main__":
+    
 
