9dd74c4c1bc5c63031439a8dd8006dba37338655
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2022-07-07 22:29:12 +0530

    added setup, utils and path

---
+++ setup.py
@@ -0,0 +1,11 @@
+from utils import *
+
+if not os.path.exists(DATA_PATH):
+    os.makedir(DATA_PATH)
+
+if not os.path.exists(MODEL_PATH):
+    os.makedir(MODEL_PATH)
+
+if not os.path.exists(TEMP_PATH):
+    os.makedir(TEMP_PATH)
+
---
+++ utils.py
@@ -0,0 +1,7 @@
+import os
+
+ROOT_PATH = os.getcwd()
+DATA_PATH = os.path.join(ROOT_PATH,"Data")
+MODEL_PATH = os.path.join(ROOT_PATH,"Model")
+TEMP_PATH = os.path.join(ROOT_PATH,"temp")
+
