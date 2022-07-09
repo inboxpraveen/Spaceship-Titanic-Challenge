da7076d277c963b76bf46ee24d6bd19aec0c13d2
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2022-07-09 15:46:03 +0530

    finalized setup.py file

--- setup.py
+++ setup.py
@@ -10,13 +10,20 @@ if not os.path.exists(MODEL_PATH):
 if not os.path.exists(TEMP_PATH):
     os.makedir(TEMP_PATH)
 
-
 def download_data(dataset_name :str = "") -> bool:
-    
-    kaggle.api.authenticate()
-    kaggle.api.dataset_download_files(dataset_name, path=DATA_PATH, unzip=True)
-
+    try:
+        kaggle.api.authenticate()
+        kaggle.api.dataset_download_files(dataset_name, path=DATA_PATH, unzip=True)
+        return True
+    except Exception as e:
+        print(f"Unable to download dataset due to: {e}")
+        return False
 
 if __name__ == "__main__":
-    ...
-    
+    input_dataset = input(f"Enter dataset name from kaggle: ")
+    is_downloaded = download_data(input_dataset)
+    if is_downloaded:
+        print(f"Successfully downloaded dataset.")
+    else:
+        print(f"Download failed. Please check if the you are authenticated properly or you typed dataset name correctly.")
+
