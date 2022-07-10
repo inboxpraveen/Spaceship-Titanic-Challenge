aad82dfc6db670cdd6411dd3898f5882095e3ed0
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2022-07-10 17:26:48 +0530

    updated preprocessing scrtipt

--- 1.DataPreProcessing.py
+++ 1.DataPreProcessing.py
@@ -71,8 +71,6 @@ def fill_values_by_based_on_mean_and_variance(df, column_name):
     total_nan = int(df[column_name].isnull().sum())
     print("Total stats: {0}, {1}, {2}".format(value_count_list,total_nan,total_rows))
     
-    
-    
 
 if __name__ == '__main__':
     train_df = pd.read_csv(data_csv_path)
